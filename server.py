097507455d26be61f2bc2d689da2c5edb135cdac
AnsonGeorge <george.anson@gmail.com>
2026-05-13 08:26:00 -0400

    server.py modified

--- server.py
+++ server.py
@@ -12,4 +12,10 @@ def emotion_analyser(): # Retrieve the text to analyze from the request argument
 # Extract the dominant value 
     dominant_value = response.split("'dominant_emotion':")[-1].strip(" '}")
 # Return a formatted string with the sentiment label and score 
-    return "For the given statement, the system response is {} The dominant emotion is {}.".format(response.split('_')[1], dominant_value)
+    return "For the given statement, the system response is {} The dominant emotion is {}.".format(response, dominant_value)
+
+@app.route("/") 
+def render_index_page(): 
+    return render_template('index.html')
+
+    
