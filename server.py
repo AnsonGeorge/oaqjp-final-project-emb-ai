3bd457485a5e7a533748f5f0b1157a493f53aa1d
AnsonGeorge <george.anson@gmail.com>
2026-05-13 08:56:25 -0400

    modified server.py

--- server.py
+++ server.py
@@ -18,4 +18,5 @@ def emotion_analyser(): # Retrieve the text to analyze from the request argument
 def render_index_page(): 
     return render_template('index.html')
 
-    
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000)
